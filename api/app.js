ed2797620bf9071408cd9ad326167eb946a807db
Hubert <goodguy.kras@gmail.com>
2023-05-04 19:25:39 +0200

    End the backend side

--- api/app.js
+++ api/app.js
@@ -1,69 +1,69 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
-const { mongoose } = require('./db/mongoose');
-
+const { mongoose } = require("./db/mongoose");
 
 // Load in the mongoose models
-const { List, Task } = require('./db/models');
-
-
+const { List, Task } = require("./db/models");
 
 /* ROUTE HANDLERS */
 
 /* LIST ROUTES */
 
-
 /**
  * GET /lists
  * Purpose: Get all list items
  */
-app.get('/lists', (req, res) => {
-    List.
-    find({}).
-    then((lists) => {
-        res.send(lists);
-    }).catch((e) => {
-        res.send(e);
+app.get("/lists", (req, res) => {
+  List.find({})
+    .then((lists) => {
+      res.send(lists);
+    })
+    .catch((e) => {
+      res.send(e);
     });
-})
+});
 
 /**
  * POST /lists
  * Purpose: Create a list
  */
-app.post('/lists', (req, res) => {
-    console.log(req.body);
-    let title = req.body.title;
-    
+app.post("/lists", (req, res) => {
+  console.log(req.body);
+  let title = req.body.title;
 
-    if (!title) {
-        return res.status(400).send('Title is required');
-    }
+  if (!title) {
+    return res.status(400).send("Title is required");
+  }
 
-    let newList = new List({
-        title
-    });
+  let newList = new List({
+    title,
+  });
 
-    newList.save().then((listDoc) => {
-        res.set('Content-Type', 'application/json');
-        res.send(listDoc);
-    }).catch((e) => {
-        console.log("There is an error creating");
-        console.log(e);
-        res.status(400).send(e);
+  newList
+    .save()
+    .then((listDoc) => {
+      res.set("Content-Type", "application/json");
+      res.send(listDoc);
+    })
+    .catch((e) => {
+      console.log("There is an error creating");
+      console.log(e);
+      res.status(400).send(e);
     });
 });
-
 
 /**
  * PATCH /lists/:id
  * Purpose: Update a specified list
  */
-app.patch('/lists/:id', (req, res) => {
-  List.findOneAndUpdate({_id: req.params.id}, {
-    $set:req.body
-  }).then(()=> {
+app.patch("/lists/:id", (req, res) => {
+  List.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: req.body,
+    }
+  ).then(() => {
     res.sendStatus(200);
   });
 });
@@ -72,28 +72,86 @@ app.patch('/lists/:id', (req, res) => {
  * DELETE /lists/:id
  * Purpose: Delete a list
  */
-app.delete('/lists/:id', (req, res) => {
-    List.findOneAndRemove({
-        _id:req.params.id
-    }).then((removedListDoc) => {
-        res.send(removedListDoc);
-    })
+app.delete("/lists/:id", (req, res) => {
+  List.findOneAndRemove({
+    _id: req.params.id,
+  }).then((removedListDoc) => {
+    res.send(removedListDoc);
+  });
 });
-
 
 /**
  * GET /lists/:listID/tasks
  * Purpose: Get all task in specified list
  */
-app.get('/lists/:listId/tasks', (req, res) => {
-    Task.find({
-        _listId: req.params.listId
-    }).then((tasks) => {
-         res.send(tasks);
-    }) 
-})
+app.get("/lists/:listId/tasks", (req, res) => {
+  Task.find({
+    _listId: req.params.listId,
+  }).then((tasks) => {
+    res.send(tasks);
+  });
+});
 
+
+/**
+ * GET /lists/:listId/tasks/:taskID
+ * Purpose: Get a one task in specified list
+ */
+app.get("/lists/:listId/tasks/:taskID", (req, res) => {
+  Task.findOne({
+    _id: req.params.taskID,
+    _listId: req.params.listId,
+  }).then((task) => {
+    res.send(task);
+  });
+});
+
+/**
+ * POST/lists/:listId/tasks
+ * Purpose: Post  a new  task in specified list
+ */
+
+app.post("/lists/:listId/tasks", (req, res) => {
+  let newTask = new Task({
+    title: req.body.title,
+    _listId: req.params.listId,
+  });
+  newTask.save().then((newTaskDoc) => {
+    res.send(newTaskDoc);
+  });
+});
+
+/**
+ * PATCH lists/:listId/tasks/:tasksId
+ * Purpose: Update a task in specified list
+ */
+app.patch("/lists/:listId/tasks/:taskId", (req, res) => {
+  Task.findOneAndUpdate(
+    {
+      _id: req.params.taskId,
+      _listId: req.params.listId,
+    },
+    {
+      $set: req.body,
+    }
+  ).then(() => {
+    res.sendStatus(200);
+  });
+});
+
+/**
+ * DELETE lists/:listId/tasks/:tasksId
+ * Purpose: DELETE a task in specified list
+ */
+app.delete("/lists/:listId/tasks/:taskId", (req, res) => {
+  Task.findOneAndRemove({
+    _id: req.params.taskId,
+    _listId: req.params.listId,
+  }).then((removedTaskDoc) => {
+    res.send(removedTaskDoc);
+  });
+});
 
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-})
+  console.log("Server is listening on port 3000");
+});
--- api/db/models/Task.model.js
+++ api/db/models/Task.model.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const TaskSchema = new mongoose.Schema({
+
+const TaskSchema = new Schema({
   title: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true,
   },
   _listId: {
     type: mongoose.Types.ObjectId,
