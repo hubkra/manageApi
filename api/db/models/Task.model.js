a8c74812088884d9fa1b6269ba02016342fe1916
Hubert <goodguy.kras@gmail.com>
2023-05-05 19:41:13 +0200

    Work in progress

--- api/db/models/Task.model.js
+++ api/db/models/Task.model.js
@@ -1,31 +1,18 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
+const TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    _listId: {
+        type: mongoose.Types.ObjectId,
+        required: true
+    }
+})
 
-const TaskSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    minlength: 1,
-  },
-  _listId: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref:'List',
-  },
-});
+const Task = mongoose.model('Task', TaskSchema);
 
-TaskSchema.index({ _listId: 1 }, { unique: true, dropDups: true });
-
-const Task = mongoose.model("Task", TaskSchema);
-
-// Assuming you want to remove the unique index on the `_listId` field
-Task.collection.dropIndex({ _listId: 1 })
-  .then(() => {
-    console.log("Index removed successfully");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-module.exports = { Task };
+module.exports = { Task }
